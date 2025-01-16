85a7b9feb779e9c29f1b63c33bae8e38fb004caf
Cameron Shaw <Cameron.shaw40@icloud.com>
2025-01-16 19:21:07 +0000

    Started GUI with tkinter

---
+++ GUI.py
@@ -0,0 +1,21 @@
+from tkinter import ttk
+
+# imports
+import ttkbootstrap as ttk
+
+
+# Window
+window = ttk.Window(title="Cara Loft Reborn", themename="darkly")
+window.resizable(True, True)
+
+
+# Variables
+
+
+# Output
+
+
+# Input
+
+
+# Window Open
